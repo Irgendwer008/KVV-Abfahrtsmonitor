ac3d41383359f6d223264538fd8fa4e867477e10
Irgendwer008 <gehamsprak@gmail.com>
2025-08-01 22:51:30 +0200

    Introduce Header  Frame

--- gui.py
+++ gui.py
@@ -1,9 +1,37 @@
-import ttkbootstrap as ttk
+from PIL import Image, ImageTk
+import tkinter as tk
+from datetime import datetime
 
 class Window:
     def __init__(self, windowconfig):
-        self.window = ttk.Toplevel()
+        self.window = tk.Toplevel()
         self.window.geometry(f"{windowconfig['width']}x{windowconfig['height']}+{windowconfig['position_x']}+{windowconfig['position_y']}")
         #self.window.attributes("-fullscreen", True)
         self.window.wm_attributes('-type', 'splash')
         self.window.bind("<Control-q>", quit)
+
+        self.stationname = tk.StringVar(value="Z10 Abfahrtsmonitor")
+        
+        self.icons = {
+            "stop": ImageTk.PhotoImage(Image.open("images/stop_icon.png").resize((60, 60))),
+        }
+
+        self.headerframe = tk.Frame(self.window, height=80, background="orange")
+        self.headerframe.pack(side="top", fill="x", expand=False, anchor="n")
+
+        self.stopiconlabel = tk.Label(self.headerframe, image=self.icons["stop"], bg="orange")
+        self.stopiconlabel.pack(side="left", padx=10, pady=10)
+        
+        self.stationlabel = tk.Label(self.headerframe, textvariable=self.stationname, font=("Helvetica", 60), anchor="w", justify="left", bg="orange")
+        self.stationlabel.pack(side="left", padx=10, pady=10)
+
+        self.timelabel = tk.Label(self.headerframe, text="", font=("Helvetica", 60), anchor="w", justify="right", bg="orange")
+        self.timelabel.pack(side="right", padx=20, pady=20)
+        def time():
+            string = datetime.now().strftime('%H:%M:%S %p')
+            self.timelabel.config(text=string)
+            self.timelabel.after(1000, time)
+        time()
+        
+        self.departuresframe = tk.Frame(self.window)
+        self.departuresframe.pack(side="top", fill="both", anchor="n", expand=True)
--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from datetime import timedelta
-import ttkbootstrap as ttk
+import tkinter as tk
 import yaml
 
-from gui import Display
+from gui import Window
 from KVV import get as get_kvv_data
 
 with open("config.yaml", "r") as file:
     config = yaml.safe_load(file)
 
-#root = ttk.Window()
-#root.withdraw()
+root = tk.Tk()
+root.withdraw()
+
+windows = []
 
 for window in config["windows"]:
-    print(window)
-#    window = Window(window)
+    windows.append(Window(window))
 
-#root.mainloop()
+root.mainloop()
 
 # response = get_kvv_data(config["credentials"]["url"], config["credentials"]["requestor_ref"], "de:08212:3", 8, timedelta(minutes=0))
 # 
