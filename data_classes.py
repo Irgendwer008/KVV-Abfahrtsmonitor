477e7fdef8ae9f17d3b1ca74eae211186e67a878
Irgendwer008 <gehamsprak@gmail.com>
2025-08-07 19:05:47 +0200

    proper docstrings and comments in data_classes.py

--- data_classes.py
+++ data_classes.py
@@ -6,17 +6,23 @@ from log import logger
     
 @dataclass
 class StopPoint:
+    """Class for one single StopPoint
+    """    
     stop_point_ref: str
     prefix: str | None
     suffix: str | None
     
 @dataclass
 class Station:
+    """Class for one Station with one or more StopPoints
+    """    
     name: str
     stop_points: list[StopPoint]
 
 @dataclass
 class Departure:
+    """Class for a Departure and all its for display necessary information
+    """    
     line_number: str
     destination: str
     platform: str
