25ff86a704efc4f0d7c171b116058bf75a865b25
Irgendwer008 <gehamsprak@gmail.com>
2025-08-12 20:22:31 +0200

    change logger to warning and to also print to stdout

--- log.py
+++ log.py
@@ -1,4 +1,6 @@
 import logging
+import sys
 
 logger = logging.getLogger("KVV-Abfahrtsmonitor")
-logging.basicConfig(format="%(levelname)s:%(asctime)s:%(name)s:%(module)s:%(lineno)s:%(message)s", filename="KVV-Abfahrtsmonitor.log", filemode="w", encoding="utf-8", level=logging.DEBUG)
+logging.basicConfig(format="%(levelname)s:%(asctime)s:%(name)s:%(module)s:%(lineno)s:%(message)s", filename="KVV-Abfahrtsmonitor.log", filemode="w", encoding="utf-8", level=logging.WARNING)
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
